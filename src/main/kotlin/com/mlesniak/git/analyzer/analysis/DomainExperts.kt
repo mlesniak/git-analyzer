f3e1343fc63cbee25ece4a2407f9ba98cb9e7bae
Michael Lesniak <mail@mlesniak.com>
2022-07-10 17:02:00 +0200

    Refactoring

--- src/main/kotlin/com/mlesniak/git/analyzer/analysis/DomainExperts.kt
+++ src/main/kotlin/com/mlesniak/git/analyzer/analysis/DomainExperts.kt
@@ -5,6 +5,7 @@ import java.io.File
 import java.util.SortedMap
 import java.util.regex.Pattern
 
+// Some helpful alias which we can't inline in the class.
 typealias Package = String
 typealias Author = String
 typealias Occurences = Int
@@ -12,10 +13,12 @@ typealias Occurences = Int
 // TODO(mlesniak) Date configuration as second parameter to allow filtering for a subset of dates.
 //                Cool date parsing using new time / date parser API
 /**
- * Map from package name to author, sorted by number of commits.
+ * Determine technical and domain experts for each package.
+ *
+ * To be useful for domain knowledge, we make the assumption that packages correspond
+ * roughly to related business domains.
  */
 class DomainExperts(private val commits: List<Commit>) {
-    // TODO(mlesniak) Domain specific class
     private var packages: MutableMap<Package, MutableMap<Author, Occurences>> = mutableMapOf()
 
     init {
@@ -24,8 +27,14 @@ class DomainExperts(private val commits: List<Commit>) {
         }
     }
 
+    /**
+     * Return a map of sorted packages (alphabetically) and for each package,
+     * a map of authors and their commits in this package (sorted by number
+     * of commits by author, ascending).
+     */
     fun get(): SortedMap<Package, SortedMap<Author, Occurences>> {
-        // There's probably a more efficient approach?
+        // Sort it once we're finished with everything. This is mainly to keep the
+        // code cleaner and shouldn't have a big performance impact.
         val sortedOccurences = packages.map { entry ->
             val m = entry.value
             val sortedOccurences = m.toSortedMap { a1, a2 -> m[a1]!!.compareTo(m[a2]!!) }
