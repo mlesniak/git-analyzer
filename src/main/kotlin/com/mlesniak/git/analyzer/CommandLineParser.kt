808ef02130438bc90de6a1ba0e96493320adc312
Michael Lesniak <mail@mlesniak.com>
2022-07-22 07:38:52 +0200

    Refactoring

---
+++ src/main/kotlin/com/mlesniak/git/analyzer/CommandLineParser.kt
@@ -0,0 +1,56 @@
+package com.mlesniak.git.analyzer
+
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import kotlinx.cli.required
+import java.nio.file.Path
+import java.time.Period
+import kotlin.io.path.isDirectory
+import kotlin.system.exitProcess
+
+enum class Analysis {
+    PackageExperts
+}
+
+data class Configuration(
+    val directory: Path,
+    val period: Period,
+    val analysis: Analysis,
+) {
+    companion object {
+        fun parse(args: Array<String>): Configuration {
+            val parser = ArgParser("git-analyzer")
+            // TODO(mlesniak) Learn how this delegation is implemented internally.
+            val directory by parser.option(
+                ArgType.String,
+                shortName = "d",
+                description = "Source code directory",
+            ).required()
+            val period by parser.option(
+                ArgType.String,
+                shortName = "p",
+                description = "Period, e.g. 1y, 2w, 2w3d, ...",
+            ).default("128y")
+            val analysis by parser.option(
+                ArgType.Choice<Analysis>(),
+                shortName = "a",
+                description = "Analysis to execute"
+            ).required()
+            parser.parse(args)
+
+            val internalPeriod = Period.parse("P$period")
+            val internalDirectory = Path.of(directory)
+            if (!internalDirectory.isDirectory()) {
+                println("Path is no a directory")
+                exitProcess(1)
+            }
+
+            return Configuration(
+                internalDirectory,
+                internalPeriod,
+                analysis,
+            )
+        }
+    }
+}
--- src/main/kotlin/com/mlesniak/git/analyzer/Main.kt
+++ src/main/kotlin/com/mlesniak/git/analyzer/Main.kt
@@ -4,63 +4,22 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.mlesniak.git.analyzer.analysis.PackageExperts
 import com.mlesniak.git.analyzer.source.GitLogParser
-import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
-import kotlinx.cli.default
-import kotlinx.cli.required
-import java.nio.file.Path
-import java.time.Period
-import kotlin.io.path.isDirectory
-import kotlin.system.exitProcess
-
-enum class Analysis {
-    PackageExperts
-}
 
 fun main(args: Array<String>) {
-    val parser = ArgParser("git-analyzer")
-    // TODO(mlesniak) Learn how this delegation is implemented internally.
-    // TODO(mlesniak) Own data class for configuration
-    val directory by parser.option(
-        ArgType.String,
-        shortName = "d",
-        description = "Source code directory",
-    ).required()
-    val period by parser.option(
-        ArgType.String,
-        shortName = "p",
-        description = "Period, e.g. 1y, 2w, 2w3d, ...",
-    ).default("128y")
-    val analysis by parser.option(
-        ArgType.Choice<Analysis>(),
-        shortName = "a",
-        description = "Analysis to execute"
-    ).required()
-    parser.parse(args)
+    val config = Configuration.parse(args)
 
-    val internalPeriod = Period.parse("P$period")
-    val internalDirectory = Path.of(directory)
-
-    if (!internalDirectory.isDirectory()) {
-        println("Path is no a directory")
-        exitProcess(1)
-    }
-    val gitLogParser = GitLogParser(internalDirectory)
+    val gitLogParser = GitLogParser(config.directory)
     val commits = gitLogParser.readRepository()
-
-    val result = when (analysis) {
-        Analysis.PackageExperts -> PackageExperts(commits, internalPeriod).get()
-        // This should never happen, since the argument
-        // parser prevents using illegal values.
-        else -> throw java.lang.IllegalStateException("Illegal analysis")
+    val result = when (config.analysis) {
+        Analysis.PackageExperts -> PackageExperts(commits, config.period).get()
     }
 
-    printResult(result)
+    printResultAsJson(result)
 }
 
-private fun printResult(result: Any) {
-    val mapper = jacksonObjectMapper()
+private fun printResultAsJson(result: Any) {
+    val json = jacksonObjectMapper()
         .configure(SerializationFeature.INDENT_OUTPUT, true)
-    val json = mapper.writeValueAsString(result)
+        .writeValueAsString(result)
     println(json)
 }
